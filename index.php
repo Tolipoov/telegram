6231a8256e2255bedb5de4609571ee5ad55b2eeb
Tolipoov <tolipovgulmurod@gmail.com>
2024-09-20 12:07:55 +0500

    second comit

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Hello";
+include 'Telegram.php';
+
+$telegram = new Telegram('7523054191:AAEbjwrWA8ftg7MR_rmDUKk9pCw-pba-VyY');
+
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
